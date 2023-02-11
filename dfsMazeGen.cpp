f5b4abb4a92ce81ea3836c6095dbf73731b8a662
J-JLucas <jlucas205@protonmail.com>
2023-02-11 16:36:50 -0700

    init

---
+++ dfsMazeGen.cpp
@@ -0,0 +1,8 @@
+// random maze procedural generator using DFS algie
+// based off of material I found here:
+// https://en.wikipedia.org/wiki/Maze_generation_algorithm
+
+int main(int argc, char *argv[]){
+	
+	return 0;
+}
